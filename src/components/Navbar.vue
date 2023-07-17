358de5e0ba8b6ff0cbfe9ed21553090d54386f33
Sajid <shojolislamshajid55@gmail.com>
2023-07-18 01:11:10 +0600

    v1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<script setup>
+    
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+            <a class="navbar-brand text-black" style="font-size: 24px; color: #ffffff; font-weight: 700;" href="#">Launch.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse my-2" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item m-2">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown m-2">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link" aria-current="page" href="#">Features</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link" aria-current="page" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link" aria-current="page" href="#">About</a>
+                    </li>
+                    <li class="nav-item m-2">
+                        <a class="nav-link" aria-current="page" href="#">Contact</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <!-- <a class="nav-link active" aria-current="page" href="#">Sign In</a> -->
+                            <button class="btn btn-ligh">Sign In</button>
+                        </li>
+                        <li class="nav-item">
+                            <!-- <a class="nav-link active" aria-current="page" href="#">Register</a> -->
+                            <button class="btn btn-primary">Register</button>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
